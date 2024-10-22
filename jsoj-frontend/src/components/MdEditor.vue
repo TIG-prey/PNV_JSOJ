da238ff52892cbb112e326a92d15b03cb0381e00
剑殇 <2201942572@qq.com>
2024-10-22 15:51:36 +0800

    完成代码编辑器的整合

---
+++ jsoj-frontend/src/components/MdEditor.vue
@@ -0,0 +1,33 @@
+<template>
+  <Editor :value="value" :plugins="plugins" @change="handleChange" />
+</template>
+
+<script setup lang="ts">
+import gfm from "@bytemd/plugin-gfm";
+import highlight from "@bytemd/plugin-highlight";
+import { Editor } from "@bytemd/vue-next";
+import { withDefaults, defineProps } from "vue";
+
+/**
+ * 定义组件属性类型，外层传入数据
+ */
+interface Props {
+  value: string;
+  handleChange: (v: string) => void;
+}
+
+const plugins = [gfm(), highlight()];
+
+/**
+ * 属性默认值
+ */
+const props = withDefaults(defineProps<Props>(), {
+  value: () => "",
+  handleChange: (v: string) => {
+    console.log(v);
+    return "";
+  },
+});
+</script>
+
+<style scoped></style>
